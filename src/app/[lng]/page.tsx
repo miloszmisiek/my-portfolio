56a259693b235dc8334a84876baccfe7b6bf542c
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2023-09-17 21:57:47 +0200

    MAJOR: add i18n with config

---
+++ src/app/[lng]/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { useTranslation } from "../i18n";
+
+export default async function Home({
+  params: { lng },
+}: {
+  params: { lng: string };
+}) {
+  const { t } = await useTranslation(lng, "translation");
+  return <div>{t("homepage")}</div>;
+}
